8868740831126dd568654fe690f6145a5afd9876
Serhii Repinskyi <115186821+serhiirepinskyi@users.noreply.github.com>
2023-07-13 01:24:44 +0300

    feat: add routes '/api/contacts' and other

--- models/contacts.js
+++ models/contacts.js
@@ -1,14 +1,60 @@
-// const fs = require('fs/promises')
+const fs = require("fs/promises");
+const path = require("path");
+const { nanoid } = require("nanoid");
 
-const listContacts = async () => {}
+const contactsPath = path.join(__dirname, "contacts.json");
 
-const getContactById = async (contactId) => {}
+// Повертає масив контактів
+const listContacts = async () => {
+  const data = await fs.readFile(contactsPath);
+  return JSON.parse(data);
+};
 
-const removeContact = async (contactId) => {}
+// Повертає об'єкт контакту з таким id
+// Повертає null, якщо контакт з таким id не знайдений
+const getContactById = async (contactId) => {
+  const allContacts = await listContacts();
+  const result = allContacts.find((item) => item.id === contactId);
+  return result || null;
+};
 
-const addContact = async (body) => {}
+// Повертає об'єкт видаленого контакту
+// Повертає null, якщо контакт з таким id не знайдений
+const removeContact = async (contactId) => {
+  const allContacts = await listContacts();
+  const index = allContacts.findIndex((item) => item.id === contactId);
+  if (index === -1) {
+    return null;
+  }
+  const [result] = allContacts.splice(index, 1);
+  await fs.writeFile(contactsPath, JSON.stringify(allContacts, null, 2));
+  return result;
+};
 
-const updateContact = async (contactId, body) => {}
+// Повертає об'єкт доданого контакту
+const addContact = async (body) => {
+  const allContacts = await listContacts();
+  const newContact = {
+    id: nanoid(),
+    ...body,
+  };
+  allContacts.push(newContact);
+  await fs.writeFile(contactsPath, JSON.stringify(allContacts, null, 2));
+  return newContact;
+};
+
+// Повертає об'єкт оновленого контакту
+// Повертає null, якщо контакт з таким id не знайдений
+const updateContact = async (contactId, body) => {
+  const allContacts = await listContacts();
+  const index = allContacts.findIndex((item) => item.id === contactId);
+  if (index === -1) {
+    return null;
+  }
+  allContacts[index] = { id: contactId, ...body };
+  await fs.writeFile(contactsPath, JSON.stringify(allContacts, null, 2));
+  return allContacts[index];
+};
 
 module.exports = {
   listContacts,
@@ -16,4 +62,4 @@ module.exports = {
   removeContact,
   addContact,
   updateContact,
-}
+};
--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -1,25 +1,96 @@
-const express = require('express')
+const express = require("express");
+const Joi = require("joi");
 
-const router = express.Router()
+const contacts = require("../../models/contacts");
 
-router.get('/', async (req, res, next) => {
-  res.json({ message: 'template message' })
-})
+const { HttpError } = require("../../helpers");
 
-router.get('/:contactId', async (req, res, next) => {
-  res.json({ message: 'template message' })
-})
+const router = express.Router();
 
-router.post('/', async (req, res, next) => {
-  res.json({ message: 'template message' })
-})
+// Joi-schema - опис вимог до об'єкту
+const addSchema = Joi.object({
+  name: Joi.string().required().messages({
+    "any.required": "Missing required name field",
+    "string.empty": "Name cannot be empty",
+  }),
+  email: Joi.string().required().messages({
+    "any.required": "Missing required email field",
+    "string.empty": "Email cannot be empty",
+  }),
+  phone: Joi.string().required().messages({
+    "any.required": "Missing required phone field",
+    "string.empty": "Phone cannot be empty",
+  }), // Додати регулярний вираз
+});
 
-router.delete('/:contactId', async (req, res, next) => {
-  res.json({ message: 'template message' })
-})
+router.get("/", async (req, res, next) => {
+  try {
+    const result = await contacts.listContacts();
+    res.json(result);
+  } catch (error) {
+    next(error);
+  }
+});
 
-router.put('/:contactId', async (req, res, next) => {
-  res.json({ message: 'template message' })
-})
+router.get("/:contactId", async (req, res, next) => {
+  try {
+    const { contactId } = req.params;
+    const result = await contacts.getContactById(contactId);
+    if (!result) {
+      throw HttpError(404, "Not found"); // throw - генерування помилки, перехід одразу на catch
+    }
+    res.json(result);
+  } catch (error) {
+    next(error);
+  }
+});
 
-module.exports = router
+router.post("/", async (req, res, next) => {
+  try {
+    const { error } = addSchema.validate(req.body); // Метод validate перевіряє, чи об'єкт відповідає Joi-schema
+    if (error) {
+      throw HttpError(400, error.message);
+    }
+    const result = await contacts.addContact(req.body);
+    res.status(201).json(result);
+  } catch (error) {
+    next(error);
+  }
+});
+
+router.delete("/:contactId", async (req, res, next) => {
+  try {
+    const { contactId } = req.params;
+    const result = await contacts.removeContact(contactId);
+    if (!result) {
+      throw HttpError(404, "Not found"); // throw - генерування помилки, перехід одразу на catch
+    }
+    res.json({ message: "Сontact deleted" }); // 1 варіант
+    // res.status(204).send(); // 2 варіант - при 204 - No Content, тіло не відправляється
+    // res.json(result); // 3 варіант
+  } catch (error) {
+    next(error);
+  }
+});
+
+router.put("/:contactId", async (req, res, next) => {
+  try {
+    if (!req.body) {
+      throw HttpError(400, "Missing fields");
+    }
+    const { error } = addSchema.validate(req.body); // Метод validate перевіряє, чи об'єкт відповідає Joi-schema
+    if (error) {
+      throw HttpError(400, error.message);
+    }
+    const { contactId } = req.params;
+    const result = await contacts.updateContact(contactId, req.body);
+    if (!result) {
+      throw HttpError(404, "Not found"); // throw - генерування помилки, перехід одразу на catch
+    }
+    res.json(result);
+  } catch (error) {
+    next(error);
+  }
+});
+
+module.exports = router;
